16f40f9dddad50410d8807902924bc15accd241d
Nikita Versan <versannau@gmail.com>
2025-08-05 17:27:21 +0300

    ComentTest

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,12 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    //Ну привет привет!
+    /*/
+     * че как оно
+     * а?
+     * а?
+     /*/
     void Start()
     {
         Time.timeScale = _timeScale;
